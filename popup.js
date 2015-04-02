ccd10a0b139745a66cbe0bd6f707530a7e450f15
Yan Zverev <yanz67@gmail.com>
2015-04-02 10:22:44 -0400

    Deleted old code

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-//chrome.tabs.executeScript(null,
-//    {code:"document.getElementById('firstName').value='tertsdfrst'"});
-
-
-//chrome.tabs.executeScript(null, {file: "encdecscript.js"});
 chrome.tabs.executeScript(null, { file: "jquery-2.1.3.js" }, function() {
     chrome.tabs.executeScript(null, { file: "encdecscript.js" });
 });
