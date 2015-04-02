6d79d53216c5bb1af4345e69d406eded5aebe27b
Yan Zverev <yanz67@gmail.com>
2015-04-02 12:41:31 -0400

    Trying To Add CryptoJS

--- encdecscript.js
+++ encdecscript.js
@@ -1,3 +1,5 @@
 $(function() {
 	$('#firstName').val('sss');
 });
+
+var wordArray = CryptoJS.lib.WordArray.random(32);
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
 chrome.tabs.executeScript(null, { file: "jquery-2.1.3.js" }, function() {
-    chrome.tabs.executeScript(null, { file: "encdecscript.js" });
+    chrome.tabs.executeScript(null, { file: "encdecscript.js" },function() 
+	{chrome.tabs.executeScript(null, {file: "/CryptoJS v3.1.2/components/core-min.js"},function() 
+	{chrome.tabs.executeScript(null, {file: "/CryptoJS v3.1.2/components/aes.js"})})})
 });
