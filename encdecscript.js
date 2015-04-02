4258d49ff4c0e48abbb492346a84e16ead991fd0
Yan Zverev <yanz67@gmail.com>
2015-04-02 15:27:29 -0400

    Added CryptoJS

--- encdecscript.js
+++ encdecscript.js
@@ -1,5 +1,6 @@
 $(function() {
 	$('#firstName').val('sss');
+	var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+	alert(encrypted);
 });
 
-var wordArray = CryptoJS.lib.WordArray.random(32);
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
+//Executing encdecscript which with jquery aes.js to be able encrypt and decrept strings
 chrome.tabs.executeScript(null, { file: "jquery-2.1.3.js" }, function() {
-    chrome.tabs.executeScript(null, { file: "encdecscript.js" },function() 
-	{chrome.tabs.executeScript(null, {file: "/CryptoJS v3.1.2/components/core-min.js"},function() 
-	{chrome.tabs.executeScript(null, {file: "/CryptoJS v3.1.2/components/aes.js"})})})
-});
+    chrome.tabs.executeScript(null, { file: "/CryptoJS v3.1.2/rollups/aes.js" },function() 
+	{chrome.tabs.executeScript(null, {file: "encdecscript.js"})})});
+
