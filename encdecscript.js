95feb35dddd3865116cfebe4f9484fd3dacb0c04
Yan Zverev <yanz67@gmail.com>
2015-04-03 11:40:35 -0400

    Added Comments

--- encdecscript.js
+++ encdecscript.js
@@ -1,6 +1,6 @@
 $(function() {
-	var firstName = $('#firstName').val();
-	var encrypted = CryptoJS.AES.encrypt(firstName, passHash);
-	$('#firstName').val(encrypted);
+	var firstName = $('#firstName').val(); // gets the information from the input box
+	var encrypted = CryptoJS.AES.encrypt(firstName, passHash); //encrypts it 
+	$('#firstName').val(encrypted); //sets it back to the input box
 });
 
--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 //Executing encdecscript which with jquery aes.js to be able encrypt and decrept strings
 
 $(function() {
-	$('#save').click(handlePassword);
-	var hash = CryptoJS.SHA3("1");
+	$('#save').click(handlePassword); //add click capability to the extension popup
+	var hash = CryptoJS.SHA3("1"); //Temporary Password Will be set in settings
 	chrome.storage.sync.set({'pass': hash.toString()}, function() {
-	         // Notify that we saved.
+	         // Notify that the password is saved
 	         //alert('Settings saved');
 	       });
-	//var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
 });
 
 function handlePassword(e){
+	//Function that will handle the click
 	chrome.storage.sync.get('pass',function(objects){
 		var hashPass = objects['pass'];
-		var userInput = $('#encPassword').val();
-		var hashInput = CryptoJS.SHA3(userInput);
-		if (hashPass == hashInput){
+		var userInput = $('#encPassword').val(); //gets user input
+		var hashInput = CryptoJS.SHA3(userInput);// hash userinput 
+		if (hashPass == hashInput){ 			 //check if user enters correct password
 			encryptFunction(hashInput);
 		}else{
 			$('#pass span').text('Wrong Password!');
@@ -25,7 +25,8 @@ function handlePassword(e){
 }
 
 function encryptFunction(hashPass){
-	var passHash = CryptoJS.SHA1(hashPass);
+	var passHash = CryptoJS.SHA1(hashPass); //rehashes user input using a different SHA1
+	//Programmatic injection of scripts to encrypt or decrypt the input boxes 
 	chrome.tabs.executeScript(null, {code: 'var passHash = '+JSON.stringify(passHash.toString())}, function() {chrome.tabs.executeScript(null,
 		{ file: "jquery-2.1.3.js" }, function() {
 	    chrome.tabs.executeScript(null, { file: "/CryptoJS v3.1.2/rollups/aes.js" },function() 
